7ed4d6a0319f415f46bf377b657a534eb3b0c0b0
marketpanda <91251429+marketpanda@users.noreply.github.com>
2022-04-15 17:11:55 +0800

    Update index.js
    Draft for first deployment. Testing out servers

--- pages/index.js
+++ pages/index.js
@@ -11,9 +11,9 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Beginning of something big!" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          Materyales coming soon! <code>pages/index.js</code>
         </p>
       </main>
 
